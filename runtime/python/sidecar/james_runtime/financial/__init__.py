f82021ab139d7a4ca4f86feb96c7b1fffd5b3b20
HazeMontaya <hazemontaya@gmail.com>
2026-09-19 00:32:38 +0200

    feat(financial): expose cortex service and events

--- runtime/python/sidecar/james_runtime/financial/__init__.py
+++ runtime/python/sidecar/james_runtime/financial/__init__.py
@@ -2,9 +2,11 @@
 from .schemas import ForecastRequest, ForecastPoint, ForecastResult, MarketBar, MarketSeries
 from .kronos_adapter import KronosAdapter, KronosAdapterConfig
 from .router import FinancialRouter
+from .events import FinancialEvent
+from .service import FinancialCortexService
 
 __all__ = [
     "ForecastRequest", "ForecastPoint", "ForecastResult",
     "MarketBar", "MarketSeries", "KronosAdapter", "KronosAdapterConfig",
-    "FinancialRouter",
+    "FinancialRouter", "FinancialEvent", "FinancialCortexService",
 ]
