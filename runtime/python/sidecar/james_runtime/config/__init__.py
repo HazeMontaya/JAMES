e599a362400e4751105a32ee34bee3a459538339
HazeMontaya <hazemontaya@gmail.com>
2026-09-19 00:41:14 +0200

    runtime: make llama.cpp executable and working directory configurable

--- runtime/python/sidecar/james_runtime/config/__init__.py
+++ runtime/python/sidecar/james_runtime/config/__init__.py
@@ -36,6 +36,9 @@ class VLLMConfig(BaseModel):
 
 
 class LlamaCppConfig(BaseModel):
+    executable: str = "llama-server"
+    working_directory: Optional[str] = None
+    auto_start: bool = True
     model_path: str = ".james/models/llama-3.1-8b-instruct-Q4_K_M.gguf"
     executable: str = "llama-server"
     working_directory: Optional[str] = None
