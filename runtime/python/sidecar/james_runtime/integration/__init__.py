653e54325b7a6846e28833843cd80d22b3107bec
HazeMontaya <hazemontaya@gmail.com>
2026-09-19 18:14:54 +0200

    feat(integration): export ecosystem contracts

--- runtime/python/sidecar/james_runtime/integration/__init__.py
+++ runtime/python/sidecar/james_runtime/integration/__init__.py
@@ -1,1 +1,20 @@
-# JAMES Runtime Integration
+"""Optional integrations for external agent ecosystems."""
+from james_runtime.integration.ecosystem import (
+    EcosystemRegistry,
+    IntegrationDomain,
+    IntegrationProfile,
+    PROFILES,
+    RealtimeVoiceAdapter,
+    WebResearchAdapter,
+    WorkflowAdapter,
+)
+
+__all__ = [
+    "EcosystemRegistry",
+    "IntegrationDomain",
+    "IntegrationProfile",
+    "PROFILES",
+    "RealtimeVoiceAdapter",
+    "WebResearchAdapter",
+    "WorkflowAdapter",
+]
